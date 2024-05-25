839b63c9a6446a8f00bb43f1c44f36c18f672bad
DinDjarinTheMandalorian <bobylamateur@gmail.com>
2024-05-25 09:12:51 +0200

    Delete jsIndex.js

--- jsIndex.js
+++
@@ -1,9 +0,0 @@
-function gameBTNPressed(){
-    window.location.replace("index_downloadgames.html");
-}; 
-function otherWorksBTNPressed(){
-    window.location.replace("index_otherworks.html");
-}; 
-function homeBTNPressed(){
-    window.location.replace("index.html");
-};
